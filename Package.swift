1ba1a418c18ee0299f4b626de0089fdcbc32e472
Chris Marshall <chris@riftvalleysoftware.com>
2020-06-21 17:02:37 -0400

    Added a name

--- Package.swift
+++ Package.swift
@@ -17,6 +17,7 @@ let package = Package(
     ],
     dependencies: [
         .package(
+            name: "RVS_Generic_Swift_Toolbox",
             url: "https://github.com/RiftValleySoftware/RVS_Generic_Swift_Toolbox.git",
             from: "1.2.0"
         )
