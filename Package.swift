47078410e0d99f400daba5af1068fe8b316cfbfc
Chris Marshall <chris@riftvalleysoftware.com>
2020-06-21 10:28:46 -0400

    Added some basic support fror SPM. More work, I'm sure, will be necessary

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.2
+
+import PackageDescription
+
+let package = Package(
+    name: "RVS_BlueThoth",
+    platforms: [
+        .iOS(.v11),
+        .tvOS(.v11),
+        .macOS(.v10_14),
+        .watchOS(.v5)
+    ],
+    products: [
+        .library(
+            name: "RVS-BlueThoth",
+            type: .dynamic,
+            targets: ["RVS_BlueThoth"])
+    ],
+    targets: [
+        .target(
+            name: "RVS_BlueThoth",
+            path: "./src")
+    ]
+)
