f0ed9b324781ef0fd00917a5fe9421ac55e57bef
Chris Marshall <chris@riftvalleysoftware.com>
2020-06-25 13:13:57 -0400

    Removed the toolbox dependency, as it is only used in the test harness.

--- Package.swift
+++ Package.swift
@@ -19,6 +19,7 @@ let package = Package(
     ],
     dependencies: [
         .package(
+            name: "RVS_Generic_Swift_Toolbox",
             url: "git@github.com:RiftValleySoftware/RVS_Generic_Swift_Toolbox.git",
             from: "1.2.1"
         )
