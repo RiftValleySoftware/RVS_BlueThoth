182d229ed42959e7ce855b9dcd5e98c8ae4572f6
Chris Marshall <chris@riftvalleysoftware.com>
2020-06-25 13:37:16 -0400

    OK. I think I may have FINALLY figured out the dependency issue.
    Fingers crossed...

--- Package.swift
+++ Package.swift
@@ -18,15 +18,12 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(
-            name: "RVS_Generic_Swift_Toolbox",
-            url: "git@github.com:RiftValleySoftware/RVS_Generic_Swift_Toolbox.git",
-            from: "1.2.1"
-        )
+        .package(name: "RVS_Generic_Swift_Toolbox", url: "git@github.com:RiftValleySoftware/RVS_Generic_Swift_Toolbox.git", from: "1.2.1")
     ],
     targets: [
         .target(
             name: "RVS_BlueThoth",
+            dependencies: [.product(name: "RVS-Generic-Swift-Toolbox", package: "RVS_Generic_Swift_Toolbox")],
             path: "./src/Source"
         )
     ]
