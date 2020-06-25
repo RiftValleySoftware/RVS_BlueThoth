e98c332cd4e0923b944ecb87464457139e843e97
Chris Marshall <chris@riftvalleysoftware.com>
2020-06-25 13:54:57 -0400

    Just some simple refactoring.

--- Package.swift
+++ Package.swift
@@ -11,11 +11,7 @@ let package = Package(
         .watchOS(.v5)
     ],
     products: [
-        .library(
-            name: "RVS-BlueThoth",
-            type: .dynamic,
-            targets: ["RVS_BlueThoth"]
-        )
+        .library(name: "RVS-BlueThoth", type: .dynamic, targets: ["RVS_BlueThoth"])
     ],
     dependencies: [
         .package(name: "RVS_Generic_Swift_Toolbox", url: "git@github.com:RiftValleySoftware/RVS_Generic_Swift_Toolbox.git", from: "1.2.1")
