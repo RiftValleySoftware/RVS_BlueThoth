2a7c37d3ea3a5ee4304299d7673587517ead1a2c
Chris Marshall <chris@riftvalleysoftware.com>
2020-08-07 10:58:20 -0400

    I have the basic skeleton in place for the WatchOS test harness.

---
+++ test/RVS_BlueThoth_Test_Harness_WatchOS/RVS_BlueToth_WatchOS_Test_Harness WatchKit Extension/RVS_BlueToth_WatchOS_Test_HarnessExtensionDelegate.swift
@@ -0,0 +1,61 @@
+//
+//  ExtensionDelegate.swift
+//  RVS_BlueToth_WatchOS_Test_Harness WatchKit Extension
+//
+//  Created by Chris Marshall on 8/7/20.
+//  Copyright © 2020 The Great Rift Valley Software Company. All rights reserved.
+//
+
+import WatchKit
+import RVS_BlueThoth_WatchOS
+
+/* ###################################################################################################################################### */
+// MARK: - Preferences Extension -
+/* ###################################################################################################################################### */
+extension CGA_PersistentPrefs {
+    /* ################################################################## */
+    /**
+     This is the scan criteria object to be used for filtering scans.
+     It is provided in the struct required by the Bluetooth subsystem.
+     */
+    var scanCriteria: RVS_BlueThoth.ScanCriteria! { RVS_BlueThoth.ScanCriteria(peripherals: peripheralFilterIDArray, services: serviceFilterIDArray, characteristics: characteristicFilterIDArray) }
+}
+
+/* ###################################################################################################################################### */
+// MARK: - Main Watch App Extension Delegate -
+/* ###################################################################################################################################### */
+class RVS_BlueToth_WatchOS_Test_Harness_ExtensionDelegate: NSObject, WKExtensionDelegate {
+    /* ################################################################## */
+    /**
+     */
+    func handle(_ backgroundTasks: Set<WKRefreshBackgroundTask>) {
+        // Sent when the system needs to launch the application in the background to process tasks. Tasks arrive in a set, so loop through and process each one.
+        for task in backgroundTasks {
+            // Use a switch statement to check the task type
+            switch task {
+            case let backgroundTask as WKApplicationRefreshBackgroundTask:
+                // Be sure to complete the background task once you’re done.
+                backgroundTask.setTaskCompletedWithSnapshot(false)
+            case let snapshotTask as WKSnapshotRefreshBackgroundTask:
+                // Snapshot tasks have a unique completion call, make sure to set your expiration date
+                snapshotTask.setTaskCompleted(restoredDefaultState: true, estimatedSnapshotExpiration: Date.distantFuture, userInfo: nil)
+            case let connectivityTask as WKWatchConnectivityRefreshBackgroundTask:
+                // Be sure to complete the connectivity task once you’re done.
+                connectivityTask.setTaskCompletedWithSnapshot(false)
+            case let urlSessionTask as WKURLSessionRefreshBackgroundTask:
+                // Be sure to complete the URL session task once you’re done.
+                urlSessionTask.setTaskCompletedWithSnapshot(false)
+            case let relevantShortcutTask as WKRelevantShortcutRefreshBackgroundTask:
+                // Be sure to complete the relevant-shortcut task once you're done.
+                relevantShortcutTask.setTaskCompletedWithSnapshot(false)
+            case let intentDidRunTask as WKIntentDidRunRefreshBackgroundTask:
+                // Be sure to complete the intent-did-run task once you're done.
+                intentDidRunTask.setTaskCompletedWithSnapshot(false)
+            default:
+                // make sure to complete unhandled task types
+                task.setTaskCompletedWithSnapshot(false)
+            }
+        }
+    }
+
+}
---
+++ test/RVS_BlueThoth_Test_Harness_WatchOS/RVS_BlueToth_WatchOS_Test_Harness WatchKit Extension/RVS_BlueToth_WatchOS_Test_Harness_DiscoveryInterfaceController.swift
@@ -0,0 +1,40 @@
+//
+//  InterfaceController.swift
+//  RVS_BlueToth_WatchOS_Test_Harness WatchKit Extension
+//
+//  Created by Chris Marshall on 8/7/20.
+//  Copyright © 2020 The Great Rift Valley Software Company. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+/* ###################################################################################################################################### */
+// MARK: - Main Watch App Discovery Interface Controller -
+/* ###################################################################################################################################### */
+class RVS_BlueToth_WatchOS_Test_Harness_DiscoveryInterfaceController: WKInterfaceController {
+    /* ################################################################## */
+    /**
+     */
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    /* ################################################################## */
+    /**
+     */
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    /* ################################################################## */
+    /**
+     */
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+}
