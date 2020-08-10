e7840aff884d99e469b0e5ca587f2468035ed9c2
Chris Marshall <chris@riftvalleysoftware.com>
2020-08-10 17:06:31 -0400

    I moved the advertisement strings to the main class, but I may do something different with them. We'll see...

--- Tests/RVS_BlueThoth_Test_Harness_WatchOS/RVS_BlueThoth_Test_Harness_WatchOS_Extension/RVS_BlueThoth_Test_Harness_WatchOS_DiscoveryViewController.swift
+++ Tests/RVS_BlueThoth_Test_Harness_WatchOS/RVS_BlueThoth_Test_Harness_WatchOS_Extension/RVS_BlueThoth_Test_Harness_WatchOS_DiscoveryViewController.swift
@@ -45,58 +45,6 @@ class RVS_BlueThoth_Test_Harness_WatchOS_DiscoveryViewController: RVS_BlueThoth_
 }
 
 /* ###################################################################################################################################### */
-// MARK: - Internal Instance Methods -
-/* ###################################################################################################################################### */
-extension RVS_BlueThoth_Test_Harness_WatchOS_DiscoveryViewController {
-    /* ################################################################## */
-    /**
-     This creates an Array of String, containing the advertisement data from the indexed device.
-     
-     - parameter inAdData: The advertisement data.
-     - parameter id: The ID string.
-     - parameter power: The RSSI level.
-     - returns: An Array of String, with the advertisement data in "key: value" form.
-     */
-    func createAdvertimentStringsFor(_ inAdData: RVS_BlueThoth.AdvertisementData?, id inID: String, power inPower: Int) -> [String] {
-        // This gives us a predictable order of things.
-        guard let sortedAdDataKeys = inAdData?.advertisementData.keys.sorted() else { return [] }
-        let sortedAdData: [(key: String, value: Any?)] = sortedAdDataKeys.compactMap { (key:$0, value: inAdData?.advertisementData[$0]) }
-
-        let retStr = sortedAdData.reduce("SLUG-ID".localizedVariant + ": \(inID)\n\t" + String(format: "SLUG-RSSI-LEVEL-FORMAT".localizedVariant, inPower)) { (current, next) in
-            let key = next.key.localizedVariant
-            let value = next.value
-            var ret = "\(current)\n"
-            
-            if let asStringArray = value as? [String] {
-                ret += current + asStringArray.reduce("\t\(key): ") { (current2, next2) in "\(current2)\n\(next2.localizedVariant)" }
-            } else if let value = value as? String {
-                ret += "\t\(key): \(value.localizedVariant)"
-            } else if let value = value as? Bool {
-                ret += "\t\(key): \(value ? "true" : "false")"
-            } else if let value = value as? Int {
-                ret += "\t\(key): \(value)"
-            } else if let value = value as? Double {
-                if "kCBAdvDataTimestamp" == next.key {  // If it's the timestamp, we can translate that, here.
-                    let date = Date(timeIntervalSinceReferenceDate: value)
-                    let dateFormatter = DateFormatter()
-                    dateFormatter.dateFormat = "SLUG-MAIN-LIST-DATE-FORMAT".localizedVariant
-                    let displayedDate = dateFormatter.string(from: date)
-                    ret += "\t\(key): \(displayedDate)"
-                } else {
-                    ret += "\t\(key): \(value)"
-                }
-            } else {    // Anything else is just a described instance of something or other.
-                ret += "\t\(key): \(String(describing: value))"
-            }
-            
-            return ret
-        }.split(separator: "\n").map { String($0) }
-        
-        return retStr
-    }
-}
-
-/* ###################################################################################################################################### */
 // MARK: - Overridden Base Class Methods -
 /* ###################################################################################################################################### */
 extension RVS_BlueThoth_Test_Harness_WatchOS_DiscoveryViewController {
@@ -133,7 +81,7 @@ extension RVS_BlueThoth_Test_Harness_WatchOS_DiscoveryViewController {
      This sets everything up to reflect the current state of the Central Manager.
      */
     override func updateUI() {
-        let advertisingStrings = createAdvertimentStringsFor(deviceDiscoveryData.advertisementData, id: deviceDiscoveryData.identifier, power: deviceDiscoveryData.rssi)
+        let advertisingStrings = RVS_BlueThoth_Test_Harness_WatchOS_ExtensionDelegate.createAdvertimentStringsFor(deviceDiscoveryData.advertisementData, id: deviceDiscoveryData.identifier, power: deviceDiscoveryData.rssi)
         advertisingInformationLabel.setText(advertisingStrings.joined(separator: "\n"))
     }
 }
--- Tests/RVS_BlueThoth_Test_Harness_WatchOS/RVS_BlueThoth_Test_Harness_WatchOS_Extension/RVS_BlueThoth_Test_Harness_WatchOS_ExtensionDelegate.swift
+++ Tests/RVS_BlueThoth_Test_Harness_WatchOS/RVS_BlueThoth_Test_Harness_WatchOS_Extension/RVS_BlueThoth_Test_Harness_WatchOS_ExtensionDelegate.swift
@@ -23,6 +23,18 @@ The Great Rift Valley Software Company: https://riftvalleysoftware.com
 import WatchKit
 import RVS_BlueThoth_WatchOS
 
+public extension StringProtocol {
+    /* ################################################################## */
+    /**
+     - returns: the localized string (main bundle) for this string.
+     */
+    var localizedVariant: String {
+        let ret = NSLocalizedString(String(self), comment: "")
+        
+        return ret
+    }
+}
+
 /* ###################################################################################################################################### */
 // MARK: - Preferences Extension -
 /* ###################################################################################################################################### */
@@ -95,6 +107,53 @@ extension RVS_BlueThoth_Test_Harness_WatchOS_ExtensionDelegate {
             
             inController.presentAlert(withTitle: inTitle.localizedVariant, message: inMessage.localizedVariant, preferredStyle: WKAlertControllerStyle.alert, actions: [okAction])
         }
+    }
+    
+    /* ################################################################## */
+    /**
+     This creates an Array of String, containing the advertisement data from the indexed device.
+     
+     - parameter inAdData: The advertisement data.
+     - parameter id: The ID string.
+     - parameter power: The RSSI level.
+     - returns: An Array of String, with the advertisement data in "key: value" form.
+     */
+    class func createAdvertimentStringsFor(_ inAdData: RVS_BlueThoth.AdvertisementData?, id inID: String, power inPower: Int) -> [String] {
+        // This gives us a predictable order of things.
+        guard let sortedAdDataKeys = inAdData?.advertisementData.keys.sorted() else { return [] }
+        let sortedAdData: [(key: String, value: Any?)] = sortedAdDataKeys.compactMap { (key:$0, value: inAdData?.advertisementData[$0]) }
+
+        let retStr = sortedAdData.reduce("SLUG-ID".localizedVariant + ": \(inID)\n\t" + String(format: "SLUG-RSSI-LEVEL-FORMAT".localizedVariant, inPower)) { (current, next) in
+            let key = next.key.localizedVariant
+            let value = next.value
+            var ret = "\(current)\n"
+            
+            if let asStringArray = value as? [String] {
+                ret += current + asStringArray.reduce("\t\(key): ") { (current2, next2) in "\(current2)\n\(next2.localizedVariant)" }
+            } else if let value = value as? String {
+                ret += "\t\(key): \(value.localizedVariant)"
+            } else if let value = value as? Bool {
+                ret += "\t\(key): \(value ? "true" : "false")"
+            } else if let value = value as? Int {
+                ret += "\t\(key): \(value)"
+            } else if let value = value as? Double {
+                if "kCBAdvDataTimestamp" == next.key {  // If it's the timestamp, we can translate that, here.
+                    let date = Date(timeIntervalSinceReferenceDate: value)
+                    let dateFormatter = DateFormatter()
+                    dateFormatter.dateFormat = "SLUG-MAIN-LIST-DATE-FORMAT".localizedVariant
+                    let displayedDate = dateFormatter.string(from: date)
+                    ret += "\t\(key): \(displayedDate)"
+                } else {
+                    ret += "\t\(key): \(value)"
+                }
+            } else {    // Anything else is just a described instance of something or other.
+                ret += "\t\(key): \(String(describing: value))"
+            }
+            
+            return ret
+        }.split(separator: "\n").map { String($0) }
+        
+        return retStr
     }
 }
 
