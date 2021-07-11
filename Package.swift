fe04a1f806132b546b3c6e07cb08cebbe507c506
Chris Marshall <chris@riftvalleysoftware.com>
2021-07-10 20:51:25 -0400

    Updated the generic tools module.

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(name: "RVS_Generic_Swift_Toolbox", url: "git@github.com:RiftValleySoftware/RVS_Generic_Swift_Toolbox.git", from: "1.6.1")
+        .package(name: "RVS_Generic_Swift_Toolbox", url: "git@github.com:RiftValleySoftware/RVS_Generic_Swift_Toolbox.git", from: "1.6.2")
     ],
     targets: [
         .target(
